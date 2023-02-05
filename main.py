f96e9543888c4ab49c270242bfd9486e04b637cb
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-02-05 14:36:29 -0500

    Started adding to the Probabilify class, which now is capable of building sample spaces from given observations and outcomes. Also have added factorial, and nCp and nPr functions as well as basic probability methods.

--- main.py
+++ main.py
@@ -1,46 +1,80 @@
 from statisfy import Statify
+from probabilify import Probabilify
+
+dice = [1, 2, 3, 4, 5, 6]
+die = Probabilify(dice)
+# probability of rolling a even number
+evens = [2, 4, 6]
+print(die.probability(evens))
+# probablity a 1 ginven that an odd number was rolled
+odds = [1, 3, 5]
+
+cards = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH',
+         'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD',
+         'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC',
+         'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS'
+         ]
+deck = Probabilify(cards)
+# probability of drawing a heart
+hearts = ['AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH']
+print(deck.probability(hearts))
+# if drawing two cards from a deck what is the probability of drawing a ace and face card
+aces_faces = ['AH', 'AD', 'AC', 'AS', 'JH', 'JD', 'JC', 'JS', 'QH', 'QD', 'QC', 'QS', 'KH', 'KD', 'KC', 'KS']
+# print(deck.at_most_one_joint(aces_faces, 2))
+
+# print the total permutations of a deck of cards
+print(deck.permutations())
+
+people = ['A', 'B', 'C', 'M']
+positions = 2
+hired = Probabilify(people)
+odds = hired.get_sample_space(observations=positions)
+print(odds)
+print(len(odds))
 
 
-A = Statify()
-B = Statify()
+# A = Statify()
+# B = Statify()
+#
+#
+# print(A.mean())
+# print(B.mean())
+# print(A.median())
+# print(B.median())
+# print(A.mode())
+# print(B.mode())
+# print(A.midpoint())
+# print(B.midpoint())
+# print(A.standard_deviation())
+# print(B.standard_deviation())
+# A.display_data()
+# B.display_data()
+# print(A.covariance(B))
+# print(A.correlation_coefficient(B))
+#
+# C = A + B
+# C.display_data()
+# print(C.mean())
+# print(C.median())
+# print(C.mode())
+# print(C.midpoint())
+# print(C.standard_deviation())
+# print(C.covariance(B))
+# print(C.correlation_coefficient(B))
+# print(C.covariance(A))
+# print(C.correlation_coefficient(A))
+#
+# A.histogram()
+# B.histogram()
+# C.histogram()
+# A.box_plot()
+# B.box_plot()
+# C.box_plot()
+#
+# # load the csv from statisfy\P2-Demographic-Data.csv
+# D = Statify('statisfy\P2-Demographic-Data.csv')
+# # D.display_data()
+# D.summary()
+# D.histogram(key='Birth rate')
+#
 
-
-print(A.mean())
-print(B.mean())
-print(A.median())
-print(B.median())
-print(A.mode())
-print(B.mode())
-print(A.midpoint())
-print(B.midpoint())
-print(A.standard_deviation())
-print(B.standard_deviation())
-A.display_data()
-B.display_data()
-print(A.covariance(B))
-print(A.correlation_coefficient(B))
-
-C = A + B
-C.display_data()
-print(C.mean())
-print(C.median())
-print(C.mode())
-print(C.midpoint())
-print(C.standard_deviation())
-print(C.covariance(B))
-print(C.correlation_coefficient(B))
-print(C.covariance(A))
-print(C.correlation_coefficient(A))
-
-A.histogram()
-B.histogram()
-C.histogram()
-A.box_plot()
-B.box_plot()
-C.box_plot()
-
-# load the csv from statisfy\P2-Demographic-Data.csv
-D = Statify('statisfy\P2-Demographic-Data.csv')
-# D.display_data()
-D.summary()
-D.histogram(key='Birth rate')
--- probabilify/probability.py
+++ probabilify/probability.py
@@ -1,31 +1,201 @@
+import itertools
+from itertools import combinations
 from statistics import *
 
 from statisfy import Statify
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 
 # class to turn data into a probability object, given a data set
 # this class will let us ask questions about the data to help
 # us understand how probable certaoin events are
+
 class Probabilify(Statify):
 
     def __init__(self, data):
         super().__init__(data)
         self.__data = data
 
-    # method to return the probability of an event
-    def probability(self, event):
-        # get the number of times the event occurs
-        occurrences = self.__data.count(event)
-        # get the total number of events
-        total = len(self.__data)
-        # return the probability
-        return occurrences / total
+    # method to return the data
+    @property
+    def data(self):
+        """
+        :return: the data
+        """
+        return self.__data
 
-    # method to return the probability of an event given another event
-    def conditional_probability(self, event, given):
-        # get the number of times the event occurs
-        occurrences = self.__data.count(event)
-        # get the number of times the given event occurs
-        given_occurrences = self.__data.count(given)
-        # return the probability
-        return occurrences / given_occurrences
+    def count(self, values):
+        """
+        Counts the number of times a value or set of values appear in the data set
+        :param values: the value or set of values to be counted
+        :return: the number of times the value or set of values appear in the data set
+        """
+        # if the values are not a list, make them a list
+        if not isinstance(values, list):
+            values = [values]
+        # count the number of times the values appear in the data set
+        count = 0
+        for value in values:
+            count += self.data.count(value)
+        return count
+
+    def factorial(self, n):
+        """
+        Calculates the factorial of a number
+        :param n: the number
+        :return: the factorial of the number
+        """
+        # calculate the factorial
+        factorial = 1
+        for i in range(1, n + 1):
+            factorial *= i
+        return factorial
+
+    def permutations(self, values=None):
+        """
+        Calculates the number of permutations of a set of values
+        :param values: the set of values
+        :return: the number of permutations
+        """
+        # if values is not given, use the data set
+        if values is None:
+            values = self.data
+        # if the values are not a list, make them a list
+        if not isinstance(values, list):
+            values = [values]
+        # calculate the number of permutations which is the length of the data set
+        # factorial.
+        permutations = self.factorial(len(self.data))
+        return permutations
+
+    def combinations(self, values=None):
+        """
+        Calculates the number of combinations of a set of values
+        :param values: the set of values
+        :return: the number of combinations
+        """
+        # if values is not given, use the data set
+        if values is None:
+            values = self.data
+        # if the values are not a list, make them a list
+        if not isinstance(values, list):
+            values = [values]
+        # calculate the number of combinations
+        combinations = 1
+        for value in values:
+            combinations *= self.data.count(value)
+        return combinations
+
+    def nCp(self, n, p):
+        """
+        Calculates the number of combinations of n things taken p at a time
+        :param n: the number of things
+        :param p: the number of things taken at a time
+        :return: the number of combinations
+        """
+        # calculate the number of combinations
+        combinations = self.factorial(n) / (
+                self.factorial(p) * self.factorial(n - p))
+        return combinations
+
+    def nCp_with_replacement(self, n, p):
+        """
+        Calculates the number of combinations of n things taken p at a time with replacement
+        :param n: the number of things
+        :param p: the number of things taken at a time
+        :return: the number of combinations
+        """
+        # calculate the number of combinations
+        combinations = self.factorial(n + p - 1) / (
+                self.factorial(p) * self.factorial(n - 1))
+        return combinations
+
+    def nPr(self, n, p):
+        """
+        Calculates the number of permutations of n things taken p at a time
+        :param n: the number of things
+        :param p: the number of things taken at a time
+        :return: the number of permutations
+        """
+        # calculate the number of permutations
+        permutations = self.factorial(n) / self.factorial(n - p)
+        return permutations
+
+    def nPr_with_replacement(n, p):
+        """
+        Calculates the number of permutations of n things taken p at a time with replacement
+        :param n: the number of things
+        :param p: the number of things taken at a time
+        :return: the number of permutations
+        """
+        # calculate the number of permutations
+        permutations = n ** p
+        return permutations
+
+    # method to return the probability of a given value
+    def probability(self, value, trials=1):
+        """
+        CaLculates the probablity a value or set of values will be picked  from
+        the data set over a given number of trials
+        :param value:  the value or set of values to be picked
+        :param trials: the number of trials to be run
+        :return: the probability of the value or set of values being picked
+        """
+        # get the number of times the value or values appear in the data set
+        count = self.count(value)
+        # calculate the probability
+        probability = count / (len(self.data) * trials)
+        return probability
+
+    def get_sample_space(self, observations, outcomes=None, replacement=False):
+        """
+        Calculates the sample space for a given number of observations and outcomes
+        :param replacement:
+        :param outcomes: list of possible outcomes
+        :param observations: the number of observations
+        :return: the sample space
+        """
+        #
+        # Example:
+        # Consider a situation where cars entering an intersection each could turn
+        # right, turn left, or go straight.
+        # An experiment consists of observing two vehicles moving through the intersection.
+        # How many sample points are there in the sample space?
+        # There are 3 possible outcomes for the first car and 3 possible outcomes for the second car.
+        # The sample space is the set of all possible outcomes for the experiment.
+        # The sample space for this experiment is {(right, right), (right, left), (right, straight),
+        # (left, right), (left, left), (left, straight), (straight, right), (straight, left),
+        # (straight, straight)}.
+        # There are 9 sample points in the sample space.
+
+        # get the sample space: given outcomes of 'L', 'R', 'S' and 2 observations, the sample space is
+        # [('L', 'L'), ('L', 'R'), ('L', 'S'), ('R', 'L'), ('R', 'R'), ('R', 'S'), ('S', 'L'), ('S', 'R'), ('S', 'S')]
+        # if replacement is True, the sample space is
+        if outcomes is None:
+            outcomes = self.data
+
+        sample_space = []
+        for i in range(observations):
+            sample_space.append(outcomes)
+
+        if replacement:
+            sample_space = list(itertools.product(*sample_space))
+        else:
+            sample_space = list(itertools.combinations(outcomes, observations))
+        return sample_space
+
+        # if replacement:
+        #     sample_space = list(itertools.combinations(outcomes, observations))
+        # else:
+        #     sample_space = list(
+        #         itertools.product(outcomes, repeat=observations))
+        #
+        # return sample_space
+
+    # for i in range(observations):
+    #     sample_space.append(outcomes)
+    # sample_space = list(itertools.product(*sample_space))
+    # # remove duplicates
+    # sample_space = list(set(sample_space))
+    # return sample_space
