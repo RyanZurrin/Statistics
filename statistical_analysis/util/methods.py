755c228ae4e3c9f63ea7b1a19b571e048f3ddbc0
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-02-19 17:50:49 -0500

    made the function that found all words from name more efficient as it was crshing the computer using up all the memory originally when names were more then 11 characters.

--- statistical_analysis/util/methods.py
+++ statistical_analysis/util/methods.py
@@ -1,32 +1,8 @@
 import itertools
 import time
 from pprint import pprint
-
-
-def progress_bar(current, total, bar_length=20):
-    """
-    Print a progress bar
-    :param current: current value
-    :param total: total value
-    :param bar_length: length of progress bar
-    :return: None
-    """
-    percent = float(current) * 100 / total
-    arrow = '-' * int(percent / 100 * bar_length - 1) + '>'
-    spaces = ' ' * (bar_length - len(arrow))
-    print('Progress: [{0}] {1}%'.format(arrow + spaces, int(percent)), end='\r')
-
-
-def process_running_spinner():
-    """
-    Print a spinner to show that a process is running
-    :return: None
-    """
-    spinner = itertools.cycle(['-', '/', '|', '\\'])
-    for _ in range(100):
-        print(next(spinner), end='\r')
-        time.sleep(0.1)
-
+import progressbar
+import resource
 
 def load_words_from_file(file_path):
     """
@@ -61,17 +37,39 @@ def make_words_from_string(string, display=False):
     :return: list of words that are in the dictionary
 
     """
-    words = load_words_from_file('words.txt')
-    permutations = []
-    # make all possible permutations of the string of characters using itertools
-    for i in range(1, len(string) + 1):
-        permutations.extend(list(itertools.permutations(string, i)))
-    # convert the list of tuples to a list of strings
-    permutations = [''.join(permutation) for permutation in permutations]
+    t0 = time.perf_counter()
+
+    # add progress bar to show how many permutations have been checked
+    progress = progressbar.ProgressBar()
+    widgets = [
+        ' [', progressbar.Timer(), '] ',
+        progressbar.Bar(),
+        ' (', progressbar.ETA(), ') ',
+        ]
+
+
+    # remove any spaces and make all the letters lowercase inplace
+    string = string.replace(' ', '').lower()
+
+    words_dict = load_words_from_file('words.txt')
+    # check the permutations while they are being generated and if they are a
+    # word in the dictionary then add them to the list of words if not then
+    # discard them so that larger names can be processed
+
+    # make all possible permutations of the string of characters using itertools and use a progress bar to show how many permutations have been checked
     words = [
-        permutation for permutation in permutations if is_word(
-            permutation, words)
+        ''.join(permutation) for i in progress(range(1, len(string) + 1))
+        for permutation in itertools.permutations(string, i)
+        if ''.join(permutation) in words_dict
     ]
+
+
+
+    # words = [
+    #     ''.join(permutation) for i in range(1, len(string) + 1)
+    #     for permutation in itertools.permutations(string, i)
+    #     if ''.join(permutation) in words_dict
+    # ]
 
     # now remove duplicates and sort alphabetically
     words = sorted(list(set(words)))
@@ -80,11 +78,17 @@ def make_words_from_string(string, display=False):
         # print the list of words so that there is a new line every 15 words
         for i in range(0, len(words), 15):
             print(words[i:i + 15])
-        print("Total permutations: {}".format(len(permutations)))
+        print("Total permutations: {}".format(len(words)))
         print("Total words: {}".format(len(words)))
 
+    t1 = time.perf_counter()
+    print("Time taken: {}".format(t1 - t0))
+    memMB = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024
+    print("Memory used: {} MB".format(memMB))
 
-# test the mwke_words_from_string function
+    return words
 
-string = 'davidgiblinj'
+
+string = 'David Giblin Jr'
+
 make_words_from_string(string, display=True)
